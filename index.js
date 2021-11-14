0ba11261e824751190dfae0abb2ead1a15ba3de8
Victor Kwasara <kwasaravictort@gmail.com>
2021-11-14 19:16:16 +0200

    chapter5

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express');
 const path = require('path');
 const app = new express();
 const ejs = require('ejs');
+const mongoose = require('mongoose');
+
+// connect to db
+
+mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
 // middle ware
 
