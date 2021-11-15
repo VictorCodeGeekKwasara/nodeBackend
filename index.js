e43c7dad16b37df4b8ef4c84c430ccd0c0da1046
Victor Kwasara <kwasaravictort@gmail.com>
2021-11-15 19:02:46 +0200

    Fixed the bug on posts page, added validation middleware chapter8

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const app = new express();
 const ejs = require('ejs');
 const mongoose = require('mongoose');
 const BlogPost = require('./models/BlogPost.js');
+const fileUpload = require('express-fileupload');
 
 // npm install body-parser
 
@@ -15,10 +16,19 @@ mongoose.connect('mongodb://localhost/my_database', { useNewUrlParser: true });
 
 // middle ware
 
+const validateMiddleWare = (req, res, next) => {
+	if (req.files == null || req.body.title == null || req.body.body == null) {
+		return res.redirect('/posts/new');
+	}
+	next();
+};
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(fileUpload());
+app.use('/posts/store', validateMiddleWare);
 // routes section
 
 app.get('/', async (req, res) => {
@@ -45,14 +55,23 @@ app.get('/posts/new', (req, res) => {
 	res.render('create');
 });
 
-app.post('/posts/store', async (req, res) => {
+app.post('/posts/store', (req, res) => {
 	console.log(req.body);
 
+	let image = req.files.image;
+	image.mv(
+		path.resolve(__dirname, 'public/assets/img', image.name),
+		async (error) => {
+			await BlogPost.create({
+				...req.body,
+				image: '/assets/img/' + image.name,
+			});
+
+			res.redirect('/');
+		}
+	);
+
 	// / model creates a new doc with browser data
-
-	await BlogPost.create(req.body);
-
-	res.redirect('/');
 });
 
 app.listen(4000, () => {
