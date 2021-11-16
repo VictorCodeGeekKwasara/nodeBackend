0dd5091d6d63307c697ecd3c09efb7a6a03b24c0
Victor Kwasara <kwasaravictort@gmail.com>
2021-11-16 12:58:27 +0200

    Chapter 10

---
+++ controllers/newUser.js
@@ -0,0 +1,5 @@
+const user = (req, res) => {
+	res.render('register');
+};
+
+module.exports = user;
--- index.js
+++ index.js
@@ -10,6 +10,10 @@ const getPostController = require('./controllers/getPost');
 const homeController = require('./controllers/home');
 const aboutController = require('./controllers/about');
 const contactController = require('./controllers/contact');
+const newUserController = require('./controllers/newUser');
+const storeUserController = require('./controllers/storeUser');
+const loginController = require('./controllers/login');
+const loginUserController = require('./controllers/loginUser');
 // npm install body-parser
 
 const bodyParser = require('body-parser');
@@ -33,11 +37,13 @@ app.get('/', homeController);
 app.get('/about', aboutController);
 app.get('/contact', contactController);
 app.get('/post/:id', getPostController);
-
+app.get('/auth/register', newUserController);
 app.get('/posts/new', newPostController);
+app.get('/auth/login', loginController);
 
+app.post('/users/login', loginUserController);
 app.post('/posts/store', storePostsController);
-
+app.post('/users/register', storeUserController);
 app.listen(4000, () => {
 	console.log('App listening on port 4000');
 });
